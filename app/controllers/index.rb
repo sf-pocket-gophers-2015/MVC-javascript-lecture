595ee7e2cb8012b790efa9a21f4b623b3606e746
[01;31m[K"name"[m[K: "Jordan Kamin <[01;31m[K"email"[m[K: "jordanakamin@gmail.com>
2015-05-06 10:27:44 -0700

    builds seeds new and index

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,14 @@
 get '/' do
+  @events = Event.all
   erb :index
+end
+
+get '/events/new' do
+  @event = Event.new
+end
+
+post '/events/create' do
+  event = Event.new(name: params[:name], date: params[:date])
+  event.save
+  redirect '/'
 end
