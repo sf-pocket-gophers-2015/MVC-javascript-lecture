85b0887b798d36b5655ecbd751b934db36a24a32
[01;31m[K"name"[m[K: "Jordan Kamin <[01;31m[K"email"[m[K: "jordanakamin@gmail.com>
2015-05-06 11:28:45 -0700

    starts work on js

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-
+  bindListeners()
+  console.log("load")
 });
+
+var bindListeners = function(){
+  $('.submit').bind('click', create)
+}
+
+var create = function(e){
+  console.log(e)
+  e.preventDefault()
+}
