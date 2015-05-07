d7178cfceb94111b4d0117b0c33ad6ce7db43238
Jordan Kamin <apprentice@Jordan-Ghost-Werewolf-Superman.local>
2015-05-07 11:35:56 -0700

    moves bindListeners to controller

--- public/js/application.js
+++ public/js/application.js
@@ -1,17 +1,11 @@
 $(document).ready(function() {
   var view = new View()
   var event = new Event()
-  var controller = new Controller()
-  bindListeners()
+  var controller = new Controller(event, view)
+  controller.bindListeners()
 });
 
-var bindListeners = function(){
-  $('.createForm').on('submit', function(submitEvent){
-    submitEvent.preventDefault();
-    var data = $(this).serialize();
-    create(data)
-  })
-}
+
 
 var create = function(data) {
   var request = $.ajax({
--- public/js/controller.js
+++ public/js/controller.js
@@ -1,2 +1,11 @@
-var Controller = function() {
+var Controller = function(event, view) {}
+
+Controller.prototype = {
+  bindListeners: function(){
+  $('.createForm').on('submit', function(submitEvent){
+    submitEvent.preventDefault();
+    var data = $(this).serialize();
+    create(data)
+  })
+}
 }
