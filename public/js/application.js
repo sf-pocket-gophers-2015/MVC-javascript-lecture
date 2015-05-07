cc501008d64fa74ebe67e9e3847d6daba62077f5
Jordan Kamin <apprentice@Jordan-Ghost-Werewolf-Superman.local>
2015-05-07 11:39:56 -0700

    moves ajax call to event, not working yet

--- public/js/application.js
+++ public/js/application.js
@@ -5,25 +5,6 @@ $(document).ready(function() {
   controller.bindListeners()
 });
 
-
-
-var create = function(data) {
-  var request = $.ajax({
-    url: '/events/create',
-    method: 'POST',
-    data: data,
-    dataType: 'json'
-  })
-  request.done(function(data){
-    addEvent(data)
-  })
-  request.error(function(data){
-    console.log("data not saved")
-    console.log(data.status)
-  })
-}
-
-
 var addEvent = function(data){
   $('.eventName').prepend(data.name)
   $('.eventDate').prepend(data.date)
--- public/js/event.js
+++ public/js/event.js
@@ -1,2 +1,19 @@
-var Event = function() {
+var Event = function() {}
+
+Event.prototype = {
+  create: function(data) {
+  var request = $.ajax({
+      url: '/events/create',
+      method: 'POST',
+      data: data,
+      dataType: 'json'
+    })
+  request.done(function(data){
+      addEvent(data)
+    })
+  request.error(function(data){
+    console.log("data not saved")
+    console.log(data.status)
+    })
+  }
 }
